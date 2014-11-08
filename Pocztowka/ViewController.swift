0850606c19ee09f887ea52e0b1753c7e694ba8c8
Agi1233 <agnieszka.kopec1@wp.pl>
2014-11-08 11:01:51 +0100

    first commit

--- Pocztowka/ViewController.swift
+++ Pocztowka/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var wiadomoscLabel: UILabel!
+    @IBOutlet weak var nadawcaLabel: UILabel!
+    
+    
+    @IBOutlet weak var wyślijButton: UIButton!
+    
+    
+    @IBOutlet weak var nadawcaTextField: UITextField!
+    @IBOutlet weak var wiadomośćTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +33,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func wyślijButtonPressed(sender: UIButton) {
+       wiadomoscLabel.hidden = false
+       wiadomoscLabel.text = wiadomośćTextField.text
+       wiadomośćTextField.text = ""
+        
+        nadawcaLabel.hidden = false
+        nadawcaLabel.text = nadawcaTextField.text
+        nadawcaTextField.text = ""
+        
+        wyślijButton.setTitle("Wysłano", forState: UIControlState.Normal)
 
+    
+    }
+    
+    
+
+    
 }
 
